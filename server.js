5264fb648f78cb95f9a6e09867646499bb347030
yossits <yosi.tsarfati@gmail.com>
2022-04-08 17:31:06 +0300

    new test

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
+//all routes list
 const indexRouter = require('./routes/index')
 const trainingRouter = require('./routes/training')
 const aboutRouter = require('./routes/about')
@@ -21,12 +22,14 @@ app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
 
+//database setup connection
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to Mongoose'))
 
+//all pages responed
 app.use('/', indexRouter)
 app.use('/training', trainingRouter)
 app.use('/about', aboutRouter)
